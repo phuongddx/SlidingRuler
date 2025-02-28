3914d36cac34720aceefb3664af97c17bca40aed
PhuongDoan <phuong.doanduy@axonactive.com>
2025-02-28 22:26:57 +0700

    chore: swiftformat

--- Sources/SlidingRuler/Styling/DefaultStyle/DefaultStyle.swift
+++ Sources/SlidingRuler/Styling/DefaultStyle/DefaultStyle.swift
@@ -30,7 +30,7 @@
 import SwiftUI
 
 public struct PrimarySlidingRulerStyle: SlidingRulerStyle {
-    public let cursorAlignment: VerticalAlignment = .top
+  public let cursorAlignment: VerticalAlignment = .top
 
     public func makeCellBody(configuration: SlidingRulerStyleConfiguation) -> some FractionableView {
         DefaultCellBody(mark: configuration.mark,
